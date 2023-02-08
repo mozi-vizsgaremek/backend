d4fd0e4cda4e5f9272b4afe1edce32d3eeb99ce6
leventea <leventea@tuta.io>
2023-02-08 14:50:34 +0100

    Users table migration

---
+++ migrations/V1__create_users_table.sql
@@ -0,0 +1,13 @@
+create type ROLE as enum ('customer', 'employee', 'manager', 'admin');
+
+create table users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    role ROLE NOT NULL DEFAULT 'customer',
+    totp_secret TEXT,
+
+    CONSTRAINT username_regexp CHECK (username ~ '^([A-Za-z0-9_-]){8,}$')
+)
