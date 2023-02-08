a870a2d9552285bde300ecf4020c44da022f41fc
leventea <leventea@tuta.io>
2023-02-08 15:45:48 +0100

    SwaggerUI setup

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,27 @@ import { fastify } from 'fastify'
 import { join } from 'path'
 import autoload from '@fastify/autoload'
 
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
+
 const server = fastify().withTypeProvider<TypeBoxTypeProvider>();
+
+server.register(swagger, {
+  swagger: {
+    info: {
+      title: 'Mozi vizsgaremek',
+      description: 'Karsza Levente, Klement Szabolcs, Papp Dávid'
+    }
+  }
+});
+
+server.register(swaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  }
+});
 
 server.register(autoload, {
   dir: join(__dirname, 'routes')
