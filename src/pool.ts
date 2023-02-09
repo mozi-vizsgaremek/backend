a0f8b86f3cb628e95dee51f745c9a96e6a3a70db
Levente <leventea@tuta.io>
2023-02-09 18:46:49 +0100

    Export pool variable

--- src/pool.ts
+++ src/pool.ts
@@ -1,9 +1,14 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { config } from './config';
-import { createPool } from "slonik";
+import { createPool, DatabasePool } from "slonik";
+
+// please not that this module assumes that decorate pool is called before the server starts up
+// if it's not called, things might not go the way you want them to
+export let pool: DatabasePool;
 
 export async function decoratePool(server: FastifyInstance) {
-  const pool = await createPool(config.postgresConnectionString);
+  const localPool = await createPool(config.postgresConnectionString);
+  pool = localPool;
 
   server.decorate('pool', pool);
 
