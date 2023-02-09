a8c51246b12dfccae73c58bbf1ae64093abce9e9
Levente <leventea@tuta.io>
2023-02-09 17:50:02 +0100

    Reformat based on editorconfig

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -2,18 +2,20 @@ import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { Type } from '@sinclair/typebox'
 
 export default (server: FastifyInstance, _opts: null, done: Function) => {
-    server.post('/register', {
-        schema: {
-            body: Type.Object({
-                username: Type.String(),
-                firstName: Type.String(),
-                lastName: Type.String(), 
-                password: Type.String()
-            })
-        }
-    }, async (_request: FastifyRequest, reply: FastifyReply) => {
-        reply.code(418).send({ whoami: "teapot" });
-    });
+  server.post('/register', {
+    schema: {
+      body: Type.Object({
+        username: Type.String(),
+        firstName: Type.String(),
+        lastName: Type.String(),
+        password: Type.String()
+      })
+    }
+  }, async (_request: FastifyRequest, reply: FastifyReply) => {
+    reply.code(418).send({ whoami: "teapot" });
 
-    done();
+
+  });
+
+  done();
 }
