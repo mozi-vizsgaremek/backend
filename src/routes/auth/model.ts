8c3845204e1576f0aa7547c8f9311a82e00c8cb4
leventea <leventea@tuta.io>
2023-02-10 15:44:36 +0100

    More basic model functions

--- src/routes/auth/model.ts
+++ src/routes/auth/model.ts
@@ -7,13 +7,22 @@ export async function getUser(id: string): Promise<User|null> {
                        `SELECT id, username FROM users WHERE id = ${id}`);
 }
 
+export async function getUserByNick(username: string): Promise<User|null> {
+  return pool.maybeOne(sql.type(User)
+                       `SELECT * FROM users WHERE username = ${username}`);
+}
+
 export async function createUser(user: User): Promise<void> {
   await pool.query(sql.unsafe
                    `INSERT INTO users (username, password, first_name, last_name)
                     VALUES (${user.username}, ${user.password}, ${user.firstName}, ${user.lastName})`);
 }
- 
-export async function userExists(username: string): Promise<boolean> {
+
+export async function userExistsByNick(username: string): Promise<boolean> {
   return pool.exists(sql.typeAlias('void')
                      `SELECT id FROM users WHERE username ILIKE ${username}`);
+}
+
+export async function userExists(id: string): Promise<boolean> {
+  return pool.exists(sql.typeAlias('void')`SELECT id FROM users WHERE id = ${id}`);  
 }
