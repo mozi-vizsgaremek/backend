49987a1a661199a09c530f902a804fc23e77a7d5
leventea <leventea@tuta.io>
2023-02-10 15:44:17 +0100

    More config fields

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,10 @@ dotenv.config();
 export type Config = {
     port: number,
     postgresConnectionString: string,
-    jwtSecret: string
+    jwtSecret: string,
+    jwtIssuer: string,
+    accessTokenExpiration: number, // stored in seconds
+    refreshTokenExpiration: number // ditto
 }
 
 function readVal<T>(key: string, caster: (x: string) => T, fallback: T|undefined = undefined): T {
@@ -22,5 +25,8 @@ function readVal<T>(key: string, caster: (x: string) => T, fallback: T|undefined
 export const config: Config = {
     port: readVal('PORT', Number, 8080),
     postgresConnectionString: readVal('POSTGRES_CONN_STR', String),
-    jwtSecret: readVal('JWT_SECRET', String)
+    jwtSecret: readVal('JWT_SECRET', String),
+    accessTokenExpiration: readVal('ACCESS_TOKEN_EXPIRATION', Number, 525960) * 60, // convert minutes to seconds
+    refreshTokenExpiration: readVal('REFRESH_TOKEN_EXPIRATION', Number, 5) * 60,
+    jwtIssuer: readVal('JWT_ISSUER', String, 'MoziBackend')
 }
