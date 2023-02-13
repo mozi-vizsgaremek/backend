81b672f08b07132412c1d742acd0829bfe52aa9d
leventea <leventea@tuta.io>
2023-02-13 09:20:56 +0100

    Automatically convert snake_case db fields

--- src/pool.ts
+++ src/pool.ts
@@ -1,13 +1,21 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { config } from './config';
 import { createPool, DatabasePool } from "slonik";
+import { createFieldNameTransformationInterceptor } from "slonik-interceptor-field-name-transformation";
 
 // please not that this module assumes that decorate pool is called before the server starts up
 // if it's not called, things might not go the way you want them to
 export let pool: DatabasePool;
 
 export async function decoratePool(server: FastifyInstance) {
-  const localPool = await createPool(config.postgresConnectionString);
+  const localPool = await createPool(config.postgresConnectionString, {
+    interceptors: [
+      createFieldNameTransformationInterceptor({
+        format: 'CAMEL_CASE'
+      })
+    ]
+  });
+
   pool = localPool;
 
   server.decorate('pool', pool);
