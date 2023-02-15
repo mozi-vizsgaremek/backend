5447d7a82f7b2cd13dd13da83fd685def5397027
leventea <leventea@tuta.io>
2023-02-15 13:08:25 +0100

    Fix the test endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { bearerSecurity } from '../types';
+import { bearerSecurity, requireRole } from '../types';
 
 export default (server: FastifyInstance, _opts: null, done: Function) => {
   server.get('/', {
     schema: {
       summary: 'Test endpoint',
       tags: [ 'test' ],
-      security: [ bearerSecurity ]
+      security: requireRole('customer')
     }
   }, (_request: FastifyRequest, reply: FastifyReply) => {
     reply.code(418).send({ whoami: "teapot" });
