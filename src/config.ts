980bc3ee0601105bd384d590395fa1b707582c40
Levente <leventea@tuta.io>
2023-02-20 19:03:28 +0100

    Add 'env' envvar to config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
+import { match, P } from 'ts-pattern';
 dotenv.config();
+
+export type EnvVals = 'dev'|'prod';
 
 export type Config = {
   port: number,
   postgresConnectionString: string,
   jwtSecret: string,
   jwtIssuer: string,
+  env: EnvVals,
   accessTokenExpiration: number, // stored in seconds
   refreshTokenExpiration: number // ditto
 }
@@ -22,11 +26,19 @@ function readVal<T>(key: string, caster: (x: string) => T, fallback: T | undefin
   return caster(process.env[key]!);
 }
 
+function envCaster(val: string): EnvVals {
+  return match(val)
+    .with(P.union('dev', 'prod'), () => val)
+    .with(P._, () => 'prod')
+    .run() as EnvVals;
+}
+
 export const config: Config = {
   port: readVal('PORT', Number, 8080),
   postgresConnectionString: readVal('POSTGRES_CONN_STR', String),
   jwtSecret: readVal('JWT_SECRET', String),
+  jwtIssuer: readVal('JWT_ISSUER', String, 'MoziBackend'),
+  env: readVal('ENV', envCaster, 'prod'),
   accessTokenExpiration: readVal('ACCESS_TOKEN_EXPIRATION', Number, 525960) * 60, // convert minutes to seconds
   refreshTokenExpiration: readVal('REFRESH_TOKEN_EXPIRATION', Number, 5) * 60,
-  jwtIssuer: readVal('JWT_ISSUER', String, 'MoziBackend')
 }
