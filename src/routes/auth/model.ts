5e184e29800c013cfea6c7172176456db14ec3e0
leventea <leventea@tuta.io>
2023-02-23 11:51:09 +0100

    Reformat

--- src/routes/auth/model.ts
+++ src/routes/auth/model.ts
@@ -9,24 +9,24 @@ export async function getUserCount(): Promise<number> {
 
 export async function getUser(id: string): Promise<User|null> {
   return pool.maybeOne(sql.type(User)
-                       `SELECT * FROM users WHERE id = ${id}`);
+    `SELECT * FROM users WHERE id = ${id}`);
 }
 
 export async function getUserByNick(username: string): Promise<User|null> {
   return pool.maybeOne(sql.type(User)
-                       `SELECT * FROM users WHERE username = ${username}`);
+    `SELECT * FROM users WHERE username = ${username}`);
 }
 
 export async function createUser(user: User): Promise<User> {
   return await pool.one(sql.type(User)
-                       `INSERT INTO users (username, password, first_name, last_name, role)
-                        VALUES (${user.username}, ${user.password}, ${user.firstName}, ${user.lastName}, ${user.role})
-                        RETURNING *`);
+    `INSERT INTO users (username, password, first_name, last_name, role)
+     VALUES (${user.username}, ${user.password}, ${user.firstName}, ${user.lastName}, ${user.role})
+     RETURNING *`);
 }
 
 export async function userExistsByNick(username: string): Promise<boolean> {
   return pool.exists(sql.typeAlias('void')
-                     `SELECT id FROM users WHERE username ILIKE ${username}`);
+    `SELECT id FROM users WHERE username ILIKE ${username}`);
 }
 
 export async function userExists(id: string): Promise<boolean> {
@@ -35,27 +35,27 @@ export async function userExists(id: string): Promise<boolean> {
 
 export async function hasTotpEnabled(id: string): Promise<boolean> {
   return pool.one(sql.typeAlias('bool')
-                  `SELECT totp_enabled FROM users WHERE id = ${id}`);
+    `SELECT totp_enabled FROM users WHERE id = ${id}`);
 }
 
 export async function hasTotpSecret(id: string): Promise<boolean> {
   return pool.one(sql.typeAlias('bool')
-                  `SELECT totp_secret IS NOT NULL FROM users WHERE id = ${id}`);
+    `SELECT totp_secret IS NOT NULL FROM users WHERE id = ${id}`);
 }
 
 export async function setTotpSecret(user: User, secret: TotpSecret) {
   return pool.query(sql.unsafe
-                    `UPDATE users SET totp_secret = ${secret} WHERE id = ${user.id!}`);
+    `UPDATE users SET totp_secret = ${secret} WHERE id = ${user.id!}`);
 }
 
 export async function setTotpStatus(user: User, status: boolean) {
   return pool.query(sql.unsafe
-                   `UPDATE users SET totp_enabled = ${status} WHERE id = ${user.id!}`);
+    `UPDATE users SET totp_enabled = ${status} WHERE id = ${user.id!}`);
 }
 
 export async function setPassword(user: User, hash: string) {
   return pool.query(sql.unsafe
-                    `UPDATE users SET password = ${hash} WHERE id = ${user.id!}`);
+    `UPDATE users SET password = ${hash} WHERE id = ${user.id!}`);
 }
 
 export async function deleteUser(user: User) {
