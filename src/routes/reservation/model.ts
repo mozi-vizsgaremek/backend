ae0deed6f1c2e56ca9f07b9891ef1044d89ce827
leventea <leventea@tuta.io>
2023-03-22 12:40:35 +0100

    Reservations model

---
+++ src/routes/reservation/model.ts
@@ -0,0 +1,38 @@
+import { pool } from "../../pool";
+import { sql, UUID } from "../../types";
+import { unwrapCount } from "../../utils";
+import { Reservation } from "./types";
+
+export async function createReservation(screeningId: UUID, userId: UUID): Promise<Reservation> {
+  return await pool.one(sql.type(Reservation)
+    `INSERT INTO reservations (screening_id, user_id, purchase_time)
+     VALUES (${screeningId}, ${userId}, now())
+     RETURNING *`);
+}
+
+export async function getScreeningReservationCount(screeningId: UUID): Promise<number | null> {
+  const count = await pool.maybeOne(sql.typeAlias('count')
+    `WITH pairs AS 
+       (SELECT s.id, a.seats FROM auditoriums a JOIN screenings s ON a.id = s.auditorium_id)
+     SELECT count(r.id)
+     FROM pairs p JOIN reservations r ON p.id = r.screening_id
+     WHERE p.id = ${screeningId}
+     GROUP BY p.id`);
+
+  return unwrapCount(count);
+}
+
+export async function deleteReservation(reservationId: UUID): Promise<Reservation | null> {
+  return await pool.maybeOne(sql.type(Reservation)
+    `DELETE FROM reservations WHERE id = ${reservationId} RETURNING *`);
+}
+
+export async function getReservation(reservationId: UUID): Promise<Reservation | null> {
+  return await pool.maybeOne(sql.type(Reservation)
+    `SELECT * FROM reservations WHERE id = ${reservationId}`);
+}
+
+export async function getReservations(): Promise<readonly Reservation[]> {
+  return await pool.many(sql.type(Reservation)
+    `SELECT * FROM reservations`);
+}
