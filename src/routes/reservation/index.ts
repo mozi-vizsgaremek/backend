389ddb8a8afca1fd7536d9ee9f1510cd361902d3
leventea <leventea@tuta.io>
2023-04-03 15:04:07 +0200

    Reserve seats

--- src/routes/reservation/index.ts
+++ src/routes/reservation/index.ts
@@ -2,6 +2,8 @@ import type { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { Type } from "@sinclair/typebox";
 import { requireRole, UUID } from "../../types";
 
+import * as am from '../auditorium/model';
+import * as as from '../screening/model';
 import * as m from './model';
 import * as s from './service';
 import * as t from './types';
@@ -11,7 +13,10 @@ const plugin: FastifyPluginAsyncTypebox = async (server, opts) => {
     schema: {
       summary: 'List all reservations, only returns reservations for the calling user',
       security: requireRole('customer'),
-      tags: [ 'reservation' ]
+      tags: [ 'reservation' ],
+      response: {
+        200: Type.Array(t.ReservationSchema)
+      }
     }
   }, async (req, rep) => {
     const res = await s.getReservations(req.user.id!);
@@ -23,7 +28,10 @@ const plugin: FastifyPluginAsyncTypebox = async (server, opts) => {
     schema: {
       summary: 'List all reservations, requires admin role.',
       security: requireRole('admin'),
-      tags: [ 'reservation' ]
+      tags: [ 'reservation' ],
+      response: {
+        200: Type.Array(t.ReservationSchema)
+      }
     }
   }, async (req, rep) => {
     const res = await s.getAllReservations();
@@ -33,7 +41,7 @@ const plugin: FastifyPluginAsyncTypebox = async (server, opts) => {
 
   server.post('/:id', {
     schema: {
-      summary: 'Reserve n amount of seats for movie with `id`',
+      summary: 'Reserve n amount of seats for screening with `id`',
       security: requireRole('customer'),
       tags: [ 'reservation' ],
       params: Type.Object({
@@ -44,7 +52,37 @@ const plugin: FastifyPluginAsyncTypebox = async (server, opts) => {
       })
     }
   }, async (req, rep) => {
-    
+    // race condition :)
+    // should probably use transactions here
+    // or implement this in PL/pgSQL
+    const reservationCount = await m.getScreeningReservationCount(req.params.id);
+    const screening = await as.getScreening(req.params.id);
+
+    if (!reservationCount || !screening)
+      return rep.error(404, 'No such screening');
+
+    const aud = await am.getAuditorium(screening.auditoriumId);
+
+    if (!aud)
+      return rep.error(404, 'Screening auditorium not found');
+
+    if (reservationCount >= aud.seats)
+      return rep.error(400, 'Screening sold out');
+
+    if (reservationCount + req.body.seats > aud.seats)
+      return rep.error(400, 'Not enough free seats available');
+
+    // this is an actual warcrime
+    // i need to be shot for writing this
+    // but im too lazy to write a decent solution to this :)
+    const createPromises 
+      = [...Array(req.body.seats).keys()]                           // create n length array
+      .map(() => m.createReservation(req.params.id, req.user.id!)); // insert n new rows
+      
+    // wait for all of the promises
+    const reservations = await Promise.all(createPromises);
+
+    return rep.ok(reservations);
   });
 
   server.delete('/:id', {
